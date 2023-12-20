ce483eac559deeee15793c0721f3350e18167f68
Lucy <lucy70381@kimo.com>
2023-12-20 23:19:49 +0800

    feat: add TestPasetpMaker

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,34 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/lucy70381/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetpMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+	require.NotEmpty(t, maker)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := NewPayload(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
