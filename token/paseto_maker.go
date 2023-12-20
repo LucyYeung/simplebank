81f8714f5c59d12c63e0ea4c68d508f5b25a4ac6
Lucy <lucy70381@kimo.com>
2023-12-20 23:19:17 +0800

    feat: implement CreateToken, VerifyToken

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -25,3 +25,28 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 
 	return maker, nil
 }
+
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
